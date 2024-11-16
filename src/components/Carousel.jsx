2862423bd4589da42608a5db8d6a646f02953673
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-11-16 18:01:53 +0100

    Last iteration edited

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,18 +2,29 @@ import {useState} from 'react';
 
 function Carousel (props) {
     const [index, setIndex] = useState(0);
-    const hasNext = index < props.images.length - 1;
-    function handleClick () {
+    
+    function handleClickRight () {
+        const hasNext = index < props.images.length - 1;
         if (hasNext) {
             setIndex(index +1);
         } else {
             setIndex(0);
         }
     }
+    function handleClickLeft() {
+        const hasNext = index > 0;
+        if (hasNext) {
+            setIndex(index -1);
+        } else {
+            setIndex(props.images.length - 1);
+        }
+    }
 
     return (
         <>
-        <img onClick={handleClick} src= {props.images[index]} alt = "random image"/>
+        <button onClick = {handleClickLeft}>Left</button>
+        <img src= {props.images[index]} alt = "random image"/>
+        <button onClick = {handleClickRight}>Right</button>
         </>
     )
 }
